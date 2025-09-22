2237d8c58e9d88238f4131bd703eeab90ca18c7b
mynervadev <developer@mynerva.in>
2025-09-22 13:00:03 +0530

    shop page fix

--- src/services/alldevicesApi.ts
+++ src/services/alldevicesApi.ts
@@ -1,11 +1,22 @@
 
+import axios from "axios";
 import axiosInstance from "../helpers/axios";
+import { toast } from "react-hot-toast";
+
 export const fetchAllDevices = async () => {
   try {
     const response = await axiosInstance.get(`/cart/alldevices`);
-    return response?.data?.data;
+    if(!response?.data?.success) return null;
+    return response?.data?.data
   } catch (error) {
-    console.error("Error fetching data:", error);
+    const errMsg = axios.isAxiosError(error)
+      ? error.response?.data?.data?.message ||
+        error.response?.data?.message ||
+        error.message
+      : "Something went wrong";
+
+    toast.error(errMsg);
+    return null;
 
   }
 };
