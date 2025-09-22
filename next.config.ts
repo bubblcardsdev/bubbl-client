9b96a15dbf287948f5334caa6e73e66f99f5b7b3
mynervadev <developer@mynerva.in>
2025-09-22 14:11:25 +0530

    prod s3 url updated in next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
     images: {
-    domains: ['bubbldev.s3.ap-south-1.amazonaws.com'],
+    domains: ['bubbldev.s3.ap-south-1.amazonaws.com', 'bubbls3.s3.ap-south-1.amazonaws.com'],
   },
 };
 
