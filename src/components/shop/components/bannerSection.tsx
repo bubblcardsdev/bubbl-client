fb89161d47c5382d4d3f9c52096ba170fc3d13ab
mynervadev <developer@mynerva.in>
2025-10-25 13:27:57 +0530

    shop page banner

--- src/components/shop/components/bannerSection.tsx
+++ src/components/shop/components/bannerSection.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function BannerSection() {
   return (
     <section className="w-full bg-[#F3F3F3] h-[calc(100dvh-60px)] relative overflow-hidden">
-      <h1 className="text-center font-bold text-3xl lg:text-4xl px-6 pt-[100px]">
+      <h1 className="text-center font-bold text-3xl lg:text-4xl px-6 mt-[100px]">
         Upgrade Your Networking <br /> Game Today
       </h1>
       <Image
@@ -12,7 +12,7 @@ function BannerSection() {
         alt="ProductBanner "
         height={1000}
         width={1000}
-        className="md:w-[600px] xl:w-[800px] min-w-[480px] w-full absolute bottom-0 lg:bottom-[-15%] left-1/2 -translate-x-1/2"
+        className="md:w-[700px] xl:w-[70%] min-w-[480px] w-full absolute bottom-0 lg:bottom-[-15%] xl-bottom-0 left-1/2 -translate-x-1/2"
       />
     </section>
   );
