b74a7655da6f5660d738fb1c7ffca5ef9e934e9a
tharunofficial12 <tharun@bubbl.cards>
2025-12-08 10:37:16 +0530

    env access added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,20 +6,22 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google Tag Manager Script */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=GTM-MGS55KRC'+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MGS55KRC');
-            `,
-          }}
-        />
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}' + dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+    `,
+  }}
+/>
 
         {/* Facebook Pixel */}
         <Script
