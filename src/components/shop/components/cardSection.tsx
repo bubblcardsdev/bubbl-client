2f1dac7271a63968622695275b8d922a06fcda29
kabilanBubbl <kabilangr@bubbl.cards>
2025-12-12 17:26:39 +0530

    Error Fix on Doc And CardSection

--- src/components/shop/components/cardSection.tsx
+++ src/components/shop/components/cardSection.tsx
@@ -139,7 +139,8 @@ function CardSection() {
                   type="submit"
                   onClick={() => {
                     trackButtonClick("subscribe");
-                    handleSubscribe}}
+                    handleSubscribe();
+                  }}
                   disabled={loading}
                   className="px-6 py-2 text-white bg-purple-500 rounded-lg hover:bg-purple-600 transition-colors duration-200 "
                 >
